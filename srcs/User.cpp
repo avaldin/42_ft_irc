986ae17a8826796248c3c0a4a14b8691d3820051
Thibaud Mouche <tmouche@z4r11p7.42lyon.fr>
2025-01-02 16:33:19 +0100

    FIX Command USER done correctly

--- srcs/User.cpp
+++ srcs/User.cpp
@@ -6,7 +6,7 @@
 /*   By: tmouche <tmouche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 23:18:47 by tmouche           #+#    #+#             */
-/*   Updated: 2025/01/02 14:51:52 by tmouche          ###   ########.fr       */
+/*   Updated: 2025/01/02 16:31:17 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@
 
 Server*	User::_server = Server::instantiate();
 
-std::string(User::*User::_method[5])(t_data&) = {
+void(User::*User::_method[NB_CHECK])(t_data&) = {
 	&User::checkRegistered,
 	&User::checkParams,
 	&User::checkUsernameRestriction,
-	&User::checkUsernameExist,
-	&User::checkNicknameExist};
+	&User::checkModeExist};
 
 User::User( void ) : _cmdName("User") {
 	return ;
@@ -37,13 +36,12 @@ User::~User( void ) {
 
 void	User::execute(Client& client) {
 	t_data		myData;
-	std::string	error;
 
 	myData.client = &client;
-	for (int idx = 0; idx < 4 && !error.empty(); idx++)
-		error = (this->*_method[idx])(myData);
-	if (!error.empty()) {
-		Send::ToClient(client._clientID, error);
+	for (int idx = 0; idx < NB_CHECK && myData.error.empty(); idx++)
+		(this->*_method[idx])(myData);
+	if (!myData.error.empty()) {
+		Send::ToClient(client._clientID, myData.error);
 		return ;
 	}
 	client._username = this->_username;
@@ -56,35 +54,38 @@ void	User::execute(Client& client) {
 	return ;
 }
 
-std::string	User::checkRegistered(t_data& myData) {
+void	User::checkRegistered(t_data& myData) {
 	std::string	error[3] = {ERR_NOTREGISTRATED, "", ERR_ALREADYREGISTRED};
-	return error[myData.client->status];
+	myData.error = error[myData.client->status];
+	return ;
 }
 
-std::string	User::checkParams(t_data& myData) {
-	(void)myData;
-	if (this->_username.empty())
-		return ERR_NEEDMOREPARAMS(this->_cmdName);
-	return "";
+void	User::checkParams(t_data& myData) {
+	if (this->_username.empty() || this->_mode.empty() || this->_realname.empty())
+		myData.error = ERR_NEEDMOREPARAMS(this->_cmdName);
+	return ;
 }
 
-std::string	User::checkUsernameRestriction(t_data& myData) {
-	(void)myData;
-	//Add a checker : 10 chara long max
-	// Letters (A-Z, a-z)
-	// Digits (0-9)
-	// Special characters: -, ., _, and ~
-	return "";
+bool	checkCorpus(char c) {
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' &&  c <= 'z') || (c >= '0' && c <= '9')
+		|| c == '-' || c == '.' || c == '_' || c == '~' || c == 0)
+		return true;
+	return false;
 }
 
-std::string	User::checkUsernameExist(t_data& myData) {
+void	User::checkUsernameRestriction(t_data& myData) {
 	(void)myData;
-	//Have to add a wayto check that correctly in Server
-	return "";
+
+	this->_username.resize(10);
+	for (int idx= 0; idx < 10; idx++) {
+		if (!checkCorpus(this->_username[idx]))
+			this->_username[idx] = 'A';
+	}
+	return ;
 }
 
-std::string	User::checkNicknameExist(t_data& myData) {
-	(void)myData;
-	//Have to add a wayto check that correctly in Server
-	return "";
+void	User::checkModeExist(t_data& myData) {
+	if (this->_mode.compare("0"))
+		myData.error = ERR_UMODEUNKNOWNFLAG;
+	return ;
 }
