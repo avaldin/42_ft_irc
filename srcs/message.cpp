2e4a23a3dda88f2b7235270ec4f86653cda6d678
avaldin <valdinalan.pro@gmail.com>
2025-01-06 16:20:22 +0100

    fix: my_recv now send the message without \n and \r

--- srcs/message.cpp
+++ srcs/message.cpp
@@ -6,7 +6,7 @@
 /*   By: avaldin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 16:03:42 by tmouche           #+#    #+#             */
-/*   Updated: 2024/12/02 10:13:23 by avaldin          ###   ########.fr       */
+/*   Updated: 2025/01/06 16:16:01 by avaldin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,9 @@ std::string my_recv(int id)
 		ret.append(&buffer[0], bytesReceived);
 	}
 	while(ret.find('\n') == std::string::npos);
+	ret.erase(ret.find('\n'));
+	if (ret.find('\r') != std::string::npos)
+		ret.erase((ret.find('\r')));
 	return (ret);
 }
 
